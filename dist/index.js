248e19c54224b32aae61d185a648f9c368100e24
lmoyacarias <lmoya@redabierta.hn>
2023-09-03 12:54:58 -0600

    feat: Agregando mas configuraciones iniciales

--- dist/index.js
+++ dist/index.js
@@ -5,6 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 // * Librerias
 const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
+const cors_1 = __importDefault(require("cors"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const dotenv_1 = __importDefault(require("dotenv"));
 // * Local Imports
 const main_1 = require("./routers/main");
@@ -12,6 +15,12 @@ const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
 // * Configurations
 dotenv_1.default.config();
+app.use((0, morgan_1.default)('dev'));
+// * Configurations Body-Parser
+app.use(body_parser_1.default.urlencoded({ extended: false }));
+app.use(body_parser_1.default.json());
+// * Configurations Cors
+app.use((0, cors_1.default)());
 app.get('/', (req, res) => {
     res.end('qweqwe');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 
 // * Librerias
 import express, { Express, Request, Response } from 'express';
+import morgan, { Morgan } from 'morgan'
+import cors, { CorsOptions } from 'cors'
+import bodyParser, { BodyParser } from 'body-parser'
+
+
 import dotenv from 'dotenv';
 
 // * Local Imports
@@ -11,11 +16,18 @@ const port: any = process.env.PORT || 3000
 
 // * Configurations
 dotenv.config();
+app.use(morgan('dev'))
+
+// * Configurations Body-Parser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// * Configurations Cors
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.end('qweqwe')
 })
-
 
 // * Config Router Principal
 app.use('/', routerMain)
